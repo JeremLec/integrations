ef10c59f3f76773738377ebdcefdfff0c0dbfdd7
Jeremy <leclercq.jeremy@outlook.fr>
2020-10-12 02:16:26 +0200

    display data in html by json file

--- static-job-listings-master/js/main.js
+++ static-job-listings-master/js/main.js
@@ -1,3 +1,39 @@
+class Job {
+  constructor(logo,company,position,postedAt,contract,location,role,languages){
+    this.logo = logo;
+    this.company = company;
+    this.position = position;
+    this.postedAt = postedAt;
+    this.contract = contract;
+    this.location = location;
+    this.role = role;
+    this.languages = languages;
+  }
+  structure(){
+    return `
+      <div class="job-box">
+        <img src="${this.logo}" alt="${this.company}">
+        <div>
+          <div class="name">
+            <span class="enterprise">${this.company}</span>
+          </div>
+          <div>
+            <span class="job">${this.position}</span>
+          </div>
+          <div class="details">
+            <span>${this.postedAt} • ${this.contract} • ${this.location}</span>
+          </div>
+        </div>
+        <div class="categories">
+          <span class="category">${this.role}</span>
+          <span class="category">${this.languages}</span>
+        </div>
+      </div>
+    `;
+  }
+}
+
+// TODO: tester avec Fetch()
 let requete = new XMLHttpRequest();
 requete.open('GET', '../data.json');
 requete.onload = () => {
@@ -9,21 +45,30 @@ requete.onload = () => {
 }
 requete.send();
 
+
 function dataJob(job){
   let listJobs = JSON.parse(job);
   listJobs.map(obj => {
-    let structure = document.querySelector('.job-box');
-    // console.log(structure);
-    let cloneStructure = structure.cloneNode(true);
-    let logo = cloneStructure.querySelector('.job-box > img')
-    // console.log(cloneStructure.querySelector('.job-box > img'));
-    logo.src = obj['logo'];
-    logo.alt = obj['company'];
-    // console.log(logo);
-    let enterprise = cloneStructure.querySelector('.enterprise');
-    enterprise.innerText = obj['company'];
-    let job = cloneStructure.querySelector('.job');
-    job.innerText = obj['position'];
-    document.querySelector('main').appendChild(cloneStructure);
+    let div = document.createElement('div');
+    
+    const logo = obj['logo'];
+    const company = obj['company'];
+    const position = obj['position'];
+    const postedAt = obj['postedAt'];
+    const contract = obj['contract'];
+    const location = obj['location'];
+    const role = obj['role'];
+    
+    // FIXME: fixer l'affichage des langages
+    const languages = obj['languages'];
+
+    console.log(languages);
+    
+    let job = new Job(logo,company,position,postedAt,contract,location,role,languages);
+    let structure = job.structure();
+
+    div.innerHTML = structure;
+
+    document.querySelector('main').appendChild(div);
   })
 }
