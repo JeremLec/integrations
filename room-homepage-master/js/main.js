44b436f6aef5451a73b8bdd662f6e06373b0c91b
Jeremy <leclercq.jeremy@outlook.fr>
2020-10-28 01:24:43 +0100

    feature slider algo

--- room-homepage-master/js/main.js
+++ room-homepage-master/js/main.js
@@ -9,3 +9,23 @@ let menuBtn = document.querySelector('.hamburger');
 let menuBtnClose = document.querySelector('.nav-btn-close');
 toggleMenu(menuBtn);
 toggleMenu(menuBtnClose);
+
+const images = ['../images/desktop-image-hero-1.jpg',
+  '../images/desktop-image-hero-2.jpg',
+  '../images/desktop-image-hero-3.jpg'];
+
+/*
+-------------------------------
+ACTION : angle left clic event
+-------------------------------
+INIT : compteur = 0;
+ 
+- Vérifie le compteur d'images
+  - Si compteur < images.length alors :
+    - images[compteur]
+    - compteur += 1
+  - Si compteur = 3 alors :
+    - compteur -= 1
+    - images[compteur]
+
+ */
